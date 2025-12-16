45fa8c14209635e7f9476d13e6fe7aedc95e9852
rodikirky <leona.rodenkirchen@rwth-aachen.de>
2025-12-16 16:02:44 +0100

    Cleaned up unsused imports

--- tests/pipeline/test_toy_fft.py
+++ tests/pipeline/test_toy_fft.py
@@ -3,7 +3,7 @@ import pytest
 
 from kubo.config import GridConfig, PhysicsConfig
 from kubo.grids import build_delta_z_kz_grids_fft
-from kubo.greens import realspace_greens_retarded, kspace_greens_retarded, _fourier_kz_to_z, _build_fft_Gkz_input_for_fixed_omega_kpar
+from kubo.greens import realspace_greens_retarded, _fourier_kz_to_z, _build_fft_Gkz_input_for_fixed_omega_kpar
 
 
 def _grid_small_fft() -> GridConfig:
